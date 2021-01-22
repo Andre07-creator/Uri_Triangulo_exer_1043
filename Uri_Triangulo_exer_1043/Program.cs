9617acc5d6ec0a395b788951e2d4c06bd5cff900
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2021-01-22 10:55:37 -0300

    Adicionar arquivos de projeto.

---
+++ Uri_Triangulo_exer_1043/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Uri_Triangulo_exer_1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] triangulo = Console.ReadLine().Split();
+            double A = double.Parse(triangulo[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(triangulo[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(triangulo[2], CultureInfo.InvariantCulture);
+
+            if (A + B > C)
+            {
+                if (A + C > B)
+                {
+                    if (B + C > A)
+                    {
+                        double perimetro = A + B + C;
+                        Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+                    }
+                    else
+                    {
+                        double area = ((A + B) / 2) * C;
+                        Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+        }
+    }
+}
